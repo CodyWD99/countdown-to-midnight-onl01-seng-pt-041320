9438b034543c351903b4bd450bd94a7fd2aea72f
Cody Dupuis <codydupuis99@gmail.com>
2020-04-17 22:52:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,14 @@ def countdown(number = 5)
 end
 countdown
 
+def countdown_with_sleep(number = 5)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  "#{number} SECOND(S)!"
+  sleep 1
+  number -= 1
+ end
+ "HAPPY NEW YEAR!"
+end
+countdown_with_sleep
+
